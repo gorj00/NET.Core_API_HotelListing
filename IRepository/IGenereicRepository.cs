c01ad53cb22175a8dd404c8519e6178a3666600a
Jan Gorol <os.jangorol@gmail.com>
2022-12-22 14:41:25 +0100

    repository setup

---
+++ IRepository/IGenereicRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HotelListing.IRepository
+{
+    public interface IGenereicRepository<T> where T : class 
+    {
+        Task<List<T>> GetAll(
+            Expression<Func<T, bool>> expression = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            List<string> includes = null
+        );
+
+    }
+}
